69a52b290d6f6836449d9ca451a3451c8646dd25
Titouannx <72860549+Titouannx@users.noreply.github.com>
2023-06-25 18:29:05 +0200

    revu arbo js

---
+++ js/constants.js
@@ -0,0 +1,23 @@
+const ingredientsDropdown = document.getElementById("ingredients-dropdown");
+const appliancesDropdown = document.getElementById("appliances-dropdown");
+const ustensilsDropdown = document.getElementById("ustensils-dropdown");
+
+const ingredientsTags = document.getElementById("ingredients-tags");
+const appliancesTags = document.getElementById("appliances-tags");
+const ustensilsTags = document.getElementById("ustensils-tags");
+
+const ingredientsButton = document.getElementById('ingredients-button');
+const ingredientsLabel = document.getElementById('ingredients-label');
+const ingredientsInput = document.getElementById('ingredients-search');
+
+const appliancesButton = document.getElementById('appliances-button');
+const appliancesLabel = document.getElementById('appliances-label');
+const appliancesInput = document.getElementById('appliances-search');
+
+const ustensilsButton = document.getElementById('ustensils-button');
+const ustensilsLabel = document.getElementById('ustensils-label');
+const ustensilsInput = document.getElementById('ustensils-search');
+
+const selectedTags = document.getElementById('selected-tags');
+
+const recipeContainer = document.getElementById("recipes-list");
